8dee6d767b9dc74a98f03aa1d967aa7f3443e7e8
Adeonir Kohl <adeonir@gmail.com>
2019-08-18 22:59:09 -0300

    Add insomnia dump

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import React, { useState, useEffect } from 'react'
 import API from './services/api'
 
 function App() {
-  const [data, setData] = useState({})
+  const [id, setId] = useState([])
+  const [messages, setMessages] = useState([])
+  const [responses, setResponses] = useState([])
+  const [inputs, setInputs] = useState([])
+  const [string, setString] = useState('')
+
+  const [textAnswers, setTextAnswers] = useState([])
+  const answers = {}
 
   useEffect(() => {
-    const loadBot = async () => {
-      const response = await API.post('/conversation/message', {
+    const fetchData = async () => {
+      await API.post('/conversation/message', {
         context: 'suitability',
+      }).then(result => {
+        setId(result.data.id)
+        setMessages(result.data.messages)
+        setResponses(result.data.responses)
+        setInputs(result.data.inputs)
       })
-      setData(response.data)
     }
 
-    loadBot()
+    fetchData()
   }, [])
 
-  console.log(data)
+  console.log(id)
+  console.log(messages)
+  console.log(responses)
+  console.log(inputs)
 
   return (
     <>
       <header>
         <h1>Fale com o Warren</h1>
       </header>
+      <section />
     </>
   )
 }
