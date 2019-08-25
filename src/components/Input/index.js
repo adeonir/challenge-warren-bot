247ddbc373be04fc09ae7fb72bd5876b3b145a01
Adeonir Kohl <adeonir@gmail.com>
2019-08-25 16:04:42 -0300

    Update input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const Input = () => {
-  return (
-    <S.Input type="text"/>
-  )
+const Input = ({ type, placeholder }) => {
+  return <S.Input type={type} placeholder={placeholder} />
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
 }
 
 export default Input
--- src/components/Input/styled.js
+++ src/components/Input/styled.js
@@ -1,8 +1,22 @@
 import styled from 'styled-components'
+import { lighten } from 'polished'
 
 import colors from '../../styles/colors'
 
 export const Input = styled.input`
   border: 0;
-  background: ${colors.light};
+  background: ${colors.white};
+  border-radius: 5px;
+  color: ${colors.black};
+  font-family: 'Montserrat', sans-serif;
+  font-size: 18px;
+  height: 70px;
+  display: flex;
+  padding: 0 30px;
+
+  &::placeholder {
+    color: ${lighten(0.5, colors.black)};
+    font-family: 'Montserrat', sans-serif;
+    font-size: 18px;
+  }
 `
