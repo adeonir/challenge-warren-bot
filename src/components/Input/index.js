88df13b3b7ded0264c2b500d64199f3c55f7be19
Adeonir Kohl <adeonir@gmail.com>
2019-08-25 13:06:51 -0300

    Add input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const Input = () => {
+  return (
+    <S.Input type="text"/>
+  )
+}
+
+export default Input
---
+++ src/components/Input/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+import colors from '../../styles/colors'
+
+export const Input = styled.input`
+  border: 0;
+  background: ${colors.light};
+`
