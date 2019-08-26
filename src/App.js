2e3faac1324e428157153d01811c5f62df552526
Adeonir Kohl <adeonir@gmail.com>
2019-08-26 15:07:59 -0300

    Add first api load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 
-// import API from './services/api'
+import API from './services/api'
+
 import GlobalStyles from './styles/global'
 import Header from './components/Header'
 import Chat from './components/Chat'
@@ -10,38 +11,56 @@ import Messages from './components/Messages'
 import Input from './components/Input'
 import Button from './components/Button'
 
-const conversation = [
-  {
-    owner: 'bot',
-    text: [
-      'Oi, eu sou o Warren. ^1000 Se você quer começar a investir bem e de maneira fácil, ^500 veio ao lugar certo. ^1000 Eu vou te ajudar.',
-      'Antes de começarmos, ^500 como posso te chamar?',
-    ],
-  },
-  {
-    owner: 'user',
-    text: ['Meu nome é Adeonir'],
-  },
-  {
-    owner: 'bot',
-    text: [
-      'Prazer, Adeonir. ^1000 Para ajudar você a ter os melhores rendimentos, ^500 preciso descobrir o seu perfil de investidor, ^500 então farei algumas perguntas rápidas. ^1000',
-    ],
-  },
-]
+// const conversation = [
+//   {
+//     owner: 'bot',
+//     text: [
+//       'Oi, eu sou o Warren. ^1000 Se você quer começar a investir bem e de maneira fácil, ^500 veio ao lugar certo. ^1000 Eu vou te ajudar.',
+//       'Antes de começarmos, ^500 como posso te chamar?',
+//     ],
+//   },
+//   {
+//     owner: 'user',
+//     text: ['Meu nome é Adeonir'],
+//   },
+//   {
+//     owner: 'bot',
+//     text: [
+//       'Prazer, Adeonir. ^1000 Para ajudar você a ter os melhores rendimentos, ^500 preciso descobrir o seu perfil de investidor, ^500 então farei algumas perguntas rápidas. ^1000',
+//     ],
+//   },
+// ]
 
 function App() {
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await API.post('/conversation/message', {
-  //       context: 'suitability',
-  //     }).then(result => {
-  //       console.log(result.data)
-  //     })
-  //   }
+  const messagesReducer = (state, action) => {
+    switch (action.type) {
+      case 'LOAD_CHAT':
+        return state.concat({
+          owner: 'bot',
+          text: action.text,
+        })
+      default:
+        throw new Error()
+    }
+  }
 
-  //   fetchData()
-  // }, [])
+  const [conversation, dispatch] = useReducer(messagesReducer, [])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await API.post('/conversation/message', {
+        context: 'suitability',
+      }).then(result => {
+        const { messages } = result.data
+        dispatch({
+          type: 'LOAD_CHAT',
+          text: messages.map(message => message.value),
+        })
+      })
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <>
