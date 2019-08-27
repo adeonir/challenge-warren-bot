b0fc0948a0fb0d42d4e2b2ff99a67c17952ef21f
Adeonir Kohl <adeonir@gmail.com>
2019-08-26 22:15:52 -0300

    Add change props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const Input = ({ type, placeholder }) => {
-  return <S.Input type={type} placeholder={placeholder} />
+const Input = ({ type, placeholder, onChange }) => {
+  return <S.Input type={type} placeholder={placeholder} onChange={onChange} />
 }
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default Input
