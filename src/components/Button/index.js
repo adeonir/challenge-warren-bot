7dc5cdc173f2e681a4602ee3ca62f3e6690cb028
Adeonir Kohl <adeonir@gmail.com>
2019-08-26 22:15:38 -0300

    Add click props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const Button = ({ children }) => {
+const Button = ({ children, onClick }) => {
   return (
-    <S.Button type='submit' props>
+    <S.Button type='submit' onClick={onClick}>
       {children}
     </S.Button>
   )
@@ -13,6 +13,7 @@ const Button = ({ children }) => {
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
 
 export default Button
